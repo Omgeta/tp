e17a53087e8e45e1b4ee56ce02855bc401d0f899
serny-afk <sernyuanong@gmail.com>
2026-03-17 20:40:26 +0800

    Modify list command to accept date object as argument

--- src/main/java/seedu/homechef/logic/commands/ListCommand.java
+++ src/main/java/seedu/homechef/logic/commands/ListCommand.java
@@ -3,7 +3,13 @@ package seedu.homechef.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.homechef.model.Model.PREDICATE_SHOW_ALL_ORDERS;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import seedu.homechef.model.Model;
+import seedu.homechef.model.order.Date;
+import seedu.homechef.model.order.Order;
 
 /**
  * Lists all orders in the HomeChef to the user.
@@ -11,14 +17,42 @@ import seedu.homechef.model.Model;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-
     public static final String MESSAGE_SUCCESS = "Listed all orders";
 
+    private final Optional<Date> targetDate;
+
+    public ListCommand() {
+        this.targetDate = Optional.empty();
+    }
+
+    public ListCommand(Date targetDate) {
+        requireNonNull(targetDate);
+        this.targetDate = Optional.of(targetDate);
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredOrderList(PREDICATE_SHOW_ALL_ORDERS);
+
+        Predicate<Order> predicate = targetDate
+                .<Predicate<Order>>map(targetDate -> order -> order.getDate().equals(targetDate))
+                .orElse(PREDICATE_SHOW_ALL_ORDERS);
+
+        model.updateFilteredOrderList(predicate);
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof ListCommand)) {
+            return false;
+        }
+
+        ListCommand otherListCommand = (ListCommand) other;
+        return Objects.equals(targetDate, otherListCommand.targetDate);
     }
 }
