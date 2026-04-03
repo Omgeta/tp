d3c048ca6a2554c337455b88a13465c044633495
Yixiannya <ohyixian@gmail.com>
2026-04-03 15:38:52 +0800

    Update sample quantity

--- src/main/java/seedu/homechef/model/util/SampleDataUtil.java
+++ src/main/java/seedu/homechef/model/util/SampleDataUtil.java
@@ -26,14 +26,15 @@ public class SampleDataUtil {
                 CompletionStatus.PENDING,
                 PaymentStatus.UNPAID,
                 getTagSet("friends"),
-                new Price("32.50"));
+                new Price("10.50"));
         Order bernice = new Order(new Food("Cupcakes (24pcs)"), new Customer("Bernice Yu"), new Phone("99272758"),
                 new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Date("20-03-2026"),
                 CompletionStatus.COMPLETED,
                 PaymentStatus.UNPAID,
                 getTagSet("colleagues", "friends"),
-                new Price("55.67"));
+                new Quantity(5),
+                new Price("52.50"));
         Order charlotte = new Order(new Food("Chocolate Chip Cookies (3pcs)"), new Customer("Charlotte Oliveiro"),
                 new Phone("93210283"),
                 new Email("charlotte@example.com"),
@@ -41,14 +42,14 @@ public class SampleDataUtil {
                 CompletionStatus.IN_PROGRESS,
                 PaymentStatus.PAID,
                 getTagSet("neighbours"),
-                new Price("4"));
+                new Price("10.50"));
         Order david = new Order(new Food("Cookies Assortment (50pcs)"), new Customer("David Li"), new Phone("91031282"),
                 new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Date("26-04-2026"),
                 CompletionStatus.COMPLETED,
                 PaymentStatus.UNPAID,
                 getTagSet("family"),
-                new Price("50"));
+                new Price("10.50"));
         Order irfan = new Order(new Food("Blueberry Pie"), new Customer("Irfan Ibrahim"), new Phone("92492021"),
                 new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Date("30-04-2026"),
@@ -56,7 +57,7 @@ public class SampleDataUtil {
                 PaymentStatus.PAID,
                 getTagSet("classmates"),
                 new Quantity(2),
-                new Price("15.20"));
+                new Price("21.00"));
         Order roy = new Order(new Food("Sourdough Bread (3pcs)"), new Customer("Roy Balakrishnan"),
                 new Phone("92624417"),
                 new Email("royb@example.com"),
@@ -64,7 +65,7 @@ public class SampleDataUtil {
                 CompletionStatus.IN_PROGRESS,
                 PaymentStatus.PAID,
                 getTagSet("colleagues"),
-                new Price("11.8"),
+                new Price("10.50"),
                 Optional.of(new PaymentInfo(PaymentType.BANK, null, "Singapore Bank", "1234567", null, null, null)));
         return new Order[]{alex, bernice, charlotte, david, irfan, roy};
     }
@@ -84,7 +85,7 @@ public class SampleDataUtil {
             "Sourdough Bread (3pcs)"
         };
         for (String foodName : sampleFoodNames) {
-            seedu.homechef.model.menu.Price menuPrice = new seedu.homechef.model.menu.Price("10.00");
+            seedu.homechef.model.menu.Price menuPrice = new seedu.homechef.model.menu.Price("10.50");
             menuBook.addMenuItem(new MenuItem(new MenuItemName(foodName), menuPrice, true));
         }
         return menuBook;
